f542a007ebdf24735933037971a10fb67d8df0e5
Matthew Payne <planetmattpayne@gmail.com>
2022-07-27 17:08:22 +0100

    edits

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
+# conda env: deforestationdynamics
 import pandas as pd
 import geopandas as gpd
-
 import plotly.express as px
 import plotly.graph_objects as go
 px.set_mapbox_access_token("pk.eyJ1IjoibWF0dGhld2pwYXluZSIsImEiOiJjbDN1NTk5dnEwZmVxM2VudnhmcWwwZm5yIn0.kT-2SV7qYLBV382pQJFtUw")
@@ -66,8 +66,8 @@ def deforestation_function():
                                     mapbox_style = "satellite-streets",
                                     labels = {"year": "Year of forest loss"},
                                     zoom = 11,
-                                    width = 400,
-                                    height = 400
+                                    width = 800,
+                                    height = 600
                                     )
     loss.update_traces(marker_line_width = 0)
     #loss.update_layout(title_text = "Deforestation across the study area")
@@ -90,8 +90,8 @@ def expansion_function():
                                         mapbox_style = "satellite-streets",
                                         labels = {"year_plant": "Year Planted"},
                                         zoom = 10,
-                                        width = 400,
-                                        height = 400)
+                                        width = 800,
+                                        height = 600)
     expansion.update_traces(marker_line_width = 0)
 
     # # Modify tooltip
@@ -109,11 +109,13 @@ def expansion_function():
     #expansion.update_layout(title_text = "Oil palm expansion 2000 - 2015")
 
     return expansion
-
+############################
 # build streamlit dashboard
-#st.title("this is a test streamlit title!")
-st.title("Visualising oil palm expansion and testing for promoted non-oil palm deforestation, in a study area of the Central Peruvian Amazon")
+############################
 
+#st.title("this is a test streamlit title!")
+st.title("Visualising oil palm expansion and non-oil palm deforestation")#, in the Central Peruvian Amazon")
+st.title("in the Central Peruvian Amazon ")
 with st.expander("Click here for contact details"):
     st.markdown("Contact me at matthewjpayne1@gmail.com, if you're interested in collaborations or just want to chat!  \n *I'll be sure to add more functionality to this dashboard in the future!*")
 
@@ -125,14 +127,14 @@ column1, column2 = st.columns(2)
 with column1:
     st.header("Non palm deforestation 2000 - 2015")
     st.write("This is deforestation derived from the Global Forest Watch dataset from Hansen et al., (2013).  \n **Source: Hansen/UMD/Google/USGS/NASA**")
-    st.plotly_chart(deforestation_function())
+    st.plotly_chart(deforestation_function(), use_container_width = True)
 
 with column2:
     st.header("Oil palm expansion 2000 - 2015")
     st.markdown("...and this is oil palm delineated using photo-interpretation and age estimated")
     st.write("")
     st.write("") # padding for subplot placement
-    st.plotly_chart(expansion_function())
+    st.plotly_chart(expansion_function(), use_container_width = True)
 
 
 with st.expander("Click here for the deforestation data source"):
@@ -142,7 +144,66 @@ with st.expander("Click here for the deforestation data source"):
 #     st.markdown("Málaga, N., Hergoualc’h, K., Kapp, G. et al. Variation in Vegetation and Ecosystem Carbon Stock Due to the Conversion of Disturbed Forest to Oil Palm Plantation in Peruvian Amazonia. Ecosystems 24, 351–369 (2021). Data available on-line from https://data.cifor.org/dataset.xhtml?persistentId=doi:10.17528/CIFOR/DATA.00196")
 
 st.header("Oil palm expansion and non-oil palm deforestation across the study area in the above maps")
-st.write("From the graph below, it looks like there *isn't* a statistically valid relationship between oil palm and the non-palm deforestation in the map, and true there isn't.  \n *...across the whole area in the same year.*  \n Step in spatially-fixed and time-fixed effects panel regression, which can account for spatial and temporal variation in deforestation response.  \n And that's it, for now. I'm intentionally withholding the model and plots until they are published in an academic paper.")
+st.write("From the graph below, it looks like there *isn't* a statistically valid relationship between oil palm and the non-palm deforestation in the map, and true there isn't.  \n  \n *...across the whole area in the same year.*  \n  \n Step in **spatially-fixed** and **time-fixed effects panel regression**, which can account for spatial and temporal variation in deforestation response.  \n  \n And that's it, for now. I'm intentionally withholding the model and plots until they are published in an academic paper.")
 st.caption("*As a side note - I hope to code a way for this graph to poll the bounds of the above maps and reflect the values within.*")
 #st.subheader("Oil palm expansion and non-oil palm deforestation across the study site")
+# column3 = st.columns(1)
+# with column3:
 st.plotly_chart(graphing())
+
+#st.markdown(
+# """
+# <link rel="stylesheet" href="https://mleibman.github.io/SlickGrid/slick.grid.css" type="text/css"/>
+# <link rel="stylesheet" href="https://mleibman.github.io/SlickGrid/css/smoothness/jquery-ui-1.8.16.custom.css" type="text/css"/>
+# <link rel="stylesheet" href="https://mleibman.github.io/SlickGrid/examples/examples.css" type="text/css"/>
+# <table width="100%">
+#   <tr>
+#     <td valign="top" width="50%">
+#       <div id="myGrid" style="width:600px;height:500px;"></div>
+#     </td>
+#     <td valign="top">
+#       <h2>Demonstrates:</h2>
+#       <ul>
+#         <li>basic grid with minimal configuration</li>
+#       </ul>
+#         <h2>View Source:</h2>
+#         <ul>
+#             <li><A href="https://github.com/mleibman/SlickGrid/blob/gh-pages/examples/example1-simple.html" target="_sourcewindow"> View the source for this example on Github</a></li>
+#         </ul>
+#     </td>
+#   </tr>
+# </table>
+# <script src="https://mleibman.github.io/SlickGrid/lib/jquery-1.7.min.js"></script>
+# <script src="https://mleibman.github.io/SlickGrid/lib/jquery.event.drag-2.2.js"></script>
+# <script src="https://mleibman.github.io/SlickGrid/slick.core.js"></script>
+# <script src="https://mleibman.github.io/SlickGrid/slick.grid.js"></script>
+# <script>
+#   var grid;
+#   var columns = [
+#     {id: "title", name: "Title", field: "title"},
+#     {id: "duration", name: "Duration", field: "duration"},
+#     {id: "%", name: "% Complete", field: "percentComplete"},
+#     {id: "start", name: "Start", field: "start"},
+#     {id: "finish", name: "Finish", field: "finish"},
+#     {id: "effort-driven", name: "Effort Driven", field: "effortDriven"}
+#   ];
+#   var options = {
+#     enableCellNavigation: true,
+#     enableColumnReorder: false
+#   };
+#   $(function () {
+#     var data = [];
+#     for (var i = 0; i < 500; i++) {
+#       data[i] = {
+#         title: "Task " + i,
+#         duration: "5 days",
+#         percentComplete: Math.round(Math.random() * 100),
+#         start: "01/01/2009",
+#         finish: "01/05/2009",
+#         effortDriven: (i % 5 == 0)
+#       };
+#     }
+#     grid = new Slick.Grid("#myGrid", data, columns, options);
+#   })
+# </script>
+# """, unsafe_allow_html=True)
