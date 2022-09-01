39658a0fe8a181604b8d626c36cd3a9c03bd8a79
Matthew Payne <planetmattpayne@gmail.com>
2022-09-01 15:48:02 +0100

    coplot added

--- app.py
+++ app.py
@@ -8,6 +8,34 @@ px.set_mapbox_access_token("pk.eyJ1IjoibWF0dGhld2pwYXluZSIsImEiOiJjbDN1NTk5dnEwZ
 import streamlit as st
 st.set_page_config(layout="wide")
 
+# def regression():
+#     palm = pd.read_csv("palm_areaYear2001-2015_timeseries_quadrat_25km2.csv")
+#     deforest = pd.read_csv("gfw_deforestation_with_distance_Year2001-2015_timeseries_quadrat_25km2.csv")
+#
+#
+#     palm_area_perYear = palm.groupby("year_plant", as_index = False)["areakm2"].sum()
+#     deforest_area_perYear = deforest.groupby("year", as_index = False)["areakm2"].sum()
+#         #deforest_mean_roadDist_perYear = deforest.groupby("year", as_index = False)["near_road"].mean()
+#         #deforest_mean_palmDist_perYear = deforest.groupby("year", as_index = False)["near_palm"].mean()
+#
+#     traceDeforest = go.Scatter(
+#             x = deforest_area_perYear["year"],
+#             y = deforest_area_perYear["areakm2"],
+#             name = "Non-oil palm deforestation",
+#             line = dict(color = "#DB0000"))
+#     tracePalm = go.Scatter(
+#             x = palm_area_perYear["year_plant"],
+#             y = palm_area_perYear["areakm2"],
+#             name = "Oil palm expansion",
+#             line = dict(color = "#F07605"))
+#
+#     combined = [traceDeforest, tracePalm]
+#
+#     fig = px.scatter(
+#         combined, x='x', y='x', opacity=0.65,
+#         trendline='ols', trendline_color_override='darkblue')
+#
+#     return fig
 #@st.cache
 def graphing():
     
@@ -152,67 +180,14 @@ with st.expander("Click here for the deforestation data source"):
 # with st.expander("Click here for the age estimation data source"):
 #     st.markdown("Málaga, N., Hergoualc’h, K., Kapp, G. et al. Variation in Vegetation and Ecosystem Carbon Stock Due to the Conversion of Disturbed Forest to Oil Palm Plantation in Peruvian Amazonia. Ecosystems 24, 351–369 (2021). Data available on-line from https://data.cifor.org/dataset.xhtml?persistentId=doi:10.17528/CIFOR/DATA.00196")
 
-st.header("Sitewide non-oil palm deforestation is not significantly predicted by oil palm expansion")
-st.write("From the graph below, it looks like there *isn't* a statistically valid relationship between oil palm and the non-palm deforestation in the map, and true there isn't.  \n  \n *...across the whole area in the same year.*  \n  \n Step in **spatially-fixed** and **time-fixed effects panel regression**, which can account for spatial and temporal variation in deforestation response.  \n  \n And that's it, for now. I'm intentionally withholding the model and plots until they are published in an academic paper.")
-st.caption("*As a side note - I hope to code a way for this graph to poll the bounds of the above maps and reflect the values within.*")
+st.header("Sitewide non-oil palm deforestation is significantly predicted by oil palm expansion")
+st.write("From the graph below, there is a statistically significant relationship between oil palm and the non-palm deforestation in the map")#, n  \n Step in **spatially-fixed** and **time-fixed effects panel regression**, which can account for spatial and temporal variation in deforestation response.  \n  \n And that's it, for now. I'm intentionally withholding the model and plots until they are published in an academic paper.")
+#st.caption("*As a side note - I hope to code a way for this graph to poll the bounds of the above maps and reflect the values within.*")
 #st.subheader("Oil palm expansion and non-oil palm deforestation across the study site")
 # column3 = st.columns(1)
 # with column3:
 st.plotly_chart(graphing())
-
-#st.markdown(
-# """
-# <link rel="stylesheet" href="https://mleibman.github.io/SlickGrid/slick.grid.css" type="text/css"/>
-# <link rel="stylesheet" href="https://mleibman.github.io/SlickGrid/css/smoothness/jquery-ui-1.8.16.custom.css" type="text/css"/>
-# <link rel="stylesheet" href="https://mleibman.github.io/SlickGrid/examples/examples.css" type="text/css"/>
-# <table width="100%">
-#   <tr>
-#     <td valign="top" width="50%">
-#       <div id="myGrid" style="width:600px;height:500px;"></div>
-#     </td>
-#     <td valign="top">
-#       <h2>Demonstrates:</h2>
-#       <ul>
-#         <li>basic grid with minimal configuration</li>
-#       </ul>
-#         <h2>View Source:</h2>
-#         <ul>
-#             <li><A href="https://github.com/mleibman/SlickGrid/blob/gh-pages/examples/example1-simple.html" target="_sourcewindow"> View the source for this example on Github</a></li>
-#         </ul>
-#     </td>
-#   </tr>
-# </table>
-# <script src="https://mleibman.github.io/SlickGrid/lib/jquery-1.7.min.js"></script>
-# <script src="https://mleibman.github.io/SlickGrid/lib/jquery.event.drag-2.2.js"></script>
-# <script src="https://mleibman.github.io/SlickGrid/slick.core.js"></script>
-# <script src="https://mleibman.github.io/SlickGrid/slick.grid.js"></script>
-# <script>
-#   var grid;
-#   var columns = [
-#     {id: "title", name: "Title", field: "title"},
-#     {id: "duration", name: "Duration", field: "duration"},
-#     {id: "%", name: "% Complete", field: "percentComplete"},
-#     {id: "start", name: "Start", field: "start"},
-#     {id: "finish", name: "Finish", field: "finish"},
-#     {id: "effort-driven", name: "Effort Driven", field: "effortDriven"}
-#   ];
-#   var options = {
-#     enableCellNavigation: true,
-#     enableColumnReorder: false
-#   };
-#   $(function () {
-#     var data = [];
-#     for (var i = 0; i < 500; i++) {
-#       data[i] = {
-#         title: "Task " + i,
-#         duration: "5 days",
-#         percentComplete: Math.round(Math.random() * 100),
-#         start: "01/01/2009",
-#         finish: "01/05/2009",
-#         effortDriven: (i % 5 == 0)
-#       };
-#     }
-#     grid = new Slick.Grid("#myGrid", data, columns, options);
-#   })
-# </script>
-# """, unsafe_allow_html=True)
+st.write("Apologies for the pixelated image, the PIL library that reads in images doesn't scale down images well.")
+st.write("But, we can't see if there is spatial variation in the promotion responses!  \n Which is where fixed-effects regression comes in handy.")
+st.write("Apologies for the pixelated image, the PIL library that reads in images doesn't scale down well.")
+st.image("coplot_25km2_quadrats.png", width = 600, caption = "Conditional plots for y ~ x, where y is non-oil palm deforestation and x is oil palm expansion. Here, we can see how the responses differ when split by quadrat")
